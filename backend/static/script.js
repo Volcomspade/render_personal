6323de993ae3b68ee9cb2027331e8c592354f750
Ryan Younker <ryanyounker@ymail.com>
2025-07-20 12:54:11 -0700

    Update script.js

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,1 +1,8 @@
-console.log('JS loaded');
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("theme-toggle");
+  toggle.onclick = () => {
+    document.body.classList.toggle("dark");
+    localStorage.theme = document.body.classList.contains("dark") ? "dark" : "light";
+  };
+  if (localStorage.theme === "dark") document.body.classList.add("dark");
+});
